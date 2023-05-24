a582a4caa094b0dac7ffa2e7ce2ea307a56b8a3c
Sarah Peled <sarahpeled00@gmail.com>
2023-05-24 19:48:51 +0200

    last fixes

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 //SELECTS THE HAMBURGER IN THE HTML
 const hamburger = document.querySelector('.hamburger');
- //SELECTS THE OVERLAY IN THE HTML
+//SELECTS THE OVERLAY IN THE HTML
 const overlay = document.querySelector('.overlay');
 
-//EVENT LISTENER WAITS FOR EVENT(CLICK) TO HAPPEN, THEN DISPLAYS THE OVERLAY
+//EVENT LISTENER WAITS FOR EVENT(CLICK) TO HAPPEN, THEN TOGGLES THE DISPLAY OF THE OVERLAY
 hamburger.addEventListener('click', function () {
-    overlay.style.display = 'block';
-});
-
-//HIDES THE OVERLAY WHEN USER CLICKS ON IT(THE OVERLAY)
-overlay.addEventListener('click', function () {
-    overlay.style.display = 'none';
+    if (overlay.style.display === 'block') {
+        overlay.style.display = 'none';
+    } else {
+        overlay.style.display = 'block';
+    }
 });
